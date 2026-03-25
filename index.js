9f3142016df3efef7c250fe70b50a84142d5765a
Aj052001 <ajsingh052001@gmail.com>
2026-03-25 10:17:56 +0530

    add index.html

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ console.log("hello")
 
 // git commit -m"first commit"
 
+// git branch -M main
+
+// git remote add origin https://github.com/Aj052001/gitdemoproject.git
+
+//  git push -u origin main
+
 
 dfkjdsflksjdf
 lksdjflkdsj
+
+
+// update 
+
+
+// git  add .
+
+
+// git commit -m"dsfkds"
+
+
+// git push origin main
